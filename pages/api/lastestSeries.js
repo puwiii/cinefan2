1e612cce793c5e3297801eaa079f62fdad8525be
Puwiii <Sebastian.mathieur@gmail.com>
2021-10-10 16:58:34 -0300

    fix paths and eslint rules

--- pages/api/lastestSeries.js
+++ pages/api/lastestSeries.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { requests } from "../../components/requests";
+import { requests } from "../../requests";
 
 import axios from "axios";
 
