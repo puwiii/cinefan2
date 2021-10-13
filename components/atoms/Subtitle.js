0cd6aa710d2b5a0834b1b88511b76c4862136545
Puwiii <Sebastian.mathieur@gmail.com>
2021-10-13 19:25:16 -0300

    create new subtitle component

---
+++ components/atoms/Subtitle.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Subtitle = styled.h3`
+  font-size: 1.47rem;
+  letter-spacing: -0.2px;
+  color: ${({ theme }) => theme.color_text_200};
+  font-weight: 600;
+  /* font-family: "Lato", sans-serif; */
+
+  @media screen and (max-width: 768px) {
+    font-size: 1.1rem;
+  }
+`;
