868df8f3a385831a8ebe127c20809309865e46b4
Puwiii <Sebastian.mathieur@gmail.com>
2021-10-13 19:29:00 -0300

    create details component and elements

---
+++ components/pages/Details/Details.elements.js
@@ -0,0 +1,220 @@
+import styled from "styled-components";
+
+import { Container } from "../../../globalStyles";
+
+export const BackgroundPlayer = styled.div`
+  text-align: center;
+  width: 100%;
+  background: ${({ isDecorative }) => (isDecorative ? "black" : "transparent")};
+  padding: ${({ isDecorative }) => (isDecorative ? "0" : "10px 0")};
+  margin-bottom: ${({ isDecorative }) => (isDecorative ? "0" : "80px;")};
+  max-height: ${({ isDecorative }) => (isDecorative ? "50vh" : "100%")};
+  height: 100%;
+  overflow: hidden;
+  position: relative;
+
+  transition: background 1s linear;
+
+  ${({ isDecorative, theme }) =>
+    isDecorative &&
+    `
+
+    
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: block;
+    width: 100%;
+    height: 100%;
+    z-index: 1000;
+    background: linear-gradient(
+      to bottom,
+      transparent 60%,
+      rgba(0,0,0,1) 100%
+    );
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: block;
+    width: 100%;
+    height: 100%;
+    z-index: 200;
+    background: rgba(0, 0, 0, 0.7);
+  }
+  `}
+`;
+
+export const BackgroundPlayerMessage = styled.span`
+  display: flex;
+  align-items: center;
+  gap: 0.87em;
+  font-size: 1.5em;
+  font-weight: 300;
+  opacity: 0.4;
+  /* position: absolute;
+  left: 50%;
+  top: 50%; */
+  /* transform: translate(-50%, -50%); */
+  z-index: 1500;
+  height: 50vh;
+  width: 100%;
+  justify-content: center;
+
+  @media screen and (max-width: 768px) {
+    height: 30vh;
+  }
+`;
+
+export const DetailsContainer = styled(Container)`
+  display: flex;
+  flex-direction: row;
+  gap: 80px;
+  margin-top: -50px;
+  position: relative;
+  z-index: 2000;
+  padding-bottom: 60px;
+`;
+
+export const DetailsHeader = styled.header`
+  position: relative;
+  z-index: 1000;
+  /* margin-top: -50px; */
+  display: flex;
+  font-size: 2em;
+  flex-wrap: wrap;
+  align-items: center;
+  gap: 1em;
+`;
+
+export const BackgroundPlayerButton = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.87em;
+  font-size: 1.5em;
+  font-weight: 300;
+  opacity: 1;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1500;
+`;
+
+export const DetailsMain = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 4em;
+  position: relative;
+  z-index: 1000;
+`;
+
+export const DetailsPoster = styled.figure`
+  flex: 3;
+`;
+
+export const DetailsInfo = styled.section`
+  display: flex;
+  gap: 2em;
+`;
+
+export const DetailsOverview = styled.article`
+  display: flex;
+  flex-direction: column;
+  gap: 2em;
+  flex: 6;
+`;
+
+export const DetailsOverviewGenres = styled.div`
+  display: flex;
+  gap: 1em;
+`;
+
+export const DetailsAside = styled.aside`
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  border-radius: 7px;
+`;
+
+export const DetailsAsideVideos = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+`;
+
+export const Companies = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  align-items: stretch;
+`;
+
+export const Company = styled.div`
+  border-radius: 4px;
+  background: ${({ theme }) => theme.color_background_300};
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  align-items: center;
+  padding: 1em;
+`;
+
+export const CompanyImage = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: grid;
+  place-items: center;
+`;
+
+export const CompanyName = styled.span`
+  font-size: 0.8rem;
+`;
+
+export const StatusTag = styled.span`
+  align-self: flex-end;
+  border-radius: 100px;
+  font-weight: 400;
+  padding: 0.47em 0.87em;
+  position: absolute;
+  background: ${({ theme }) => theme.color_success};
+`;
+
+export const DetailsReviews = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+`;
+
+export const Review = styled.article`
+  display: flex;
+  gap: 2em;
+  border-bottom: 1px solid ${({ theme }) => theme.color_background_300};
+  padding: 1em 0;
+`;
+
+export const ReviewUser = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  align-items: center;
+`;
+
+export const UserImage = styled.figure`
+  background: ${({ theme }) => theme.color_background_300};
+  border-radius: 50%;
+  overflow: hidden;
+  width: 75px;
+  height: 75px;
+`;
+
+export const UserName = styled.span`
+  font-size: 0.6rem;
+`;
---
+++ components/pages/Details/index.js
@@ -0,0 +1,193 @@
+import { useState, useEffect } from "react";
+
+import Image from "next/image";
+
+import { FiVideoOff } from "react-icons/fi";
+
+import { FaPlay } from "react-icons/fa";
+
+import { Title } from "../../atoms/Title";
+import { Subtitle } from "../../atoms/Subtitle";
+import { Video } from "../../atoms/Video";
+import { PrimaryButton } from "../../atoms/PrimaryButton";
+import Poster from "../../molecules/Poster";
+
+import {
+  BackgroundPlayer,
+  BackgroundPlayerMessage,
+  BackgroundPlayerButton,
+  DetailsContainer,
+  DetailsHeader,
+  DetailsInfo,
+  DetailsOverview,
+  DetailsMain,
+  DetailsPoster,
+  DetailsAside,
+  DetailsAsideVideos,
+  DetailsOverviewGenres,
+  DeatilsAsideCompanies,
+  Companies,
+  Company,
+  CompanyName,
+  CompanyImage,
+  StatusTag,
+  DetailsReviews,
+  Review,
+  ReviewUser,
+  UserImage,
+  UserName,
+} from "./Details.elements";
+import { Pharagraph } from "../../atoms/Pharagraph";
+import { Rating } from "../../atoms/Rating";
+import { Tag } from "../../atoms/Tag";
+const Index = ({ mediaDetails, mediaVideos, mediaReviews, mediaType }) => {
+  console.log(mediaReviews);
+
+  const [video, setVideo] = useState(null);
+
+  const [isDecorative, setIsDecorative] = useState(true);
+
+  useEffect(() => {
+    let trailer;
+
+    trailer = mediaVideos.find((video) => {
+      return video.type === "Trailer";
+    });
+
+    if (trailer === undefined) {
+      trailer = mediaVideos.find((video) => {
+        return video.type === "Teaser";
+      });
+    }
+    setVideo(trailer);
+  }, [mediaVideos]);
+
+  return (
+    <>
+      <BackgroundPlayer isDecorative={isDecorative}>
+        {video === undefined ? (
+          <BackgroundPlayerMessage>
+            <FiVideoOff /> sin video
+          </BackgroundPlayerMessage>
+        ) : (
+          <>
+            {isDecorative && (
+              <BackgroundPlayerButton>
+                <PrimaryButton onClick={(e) => setIsDecorative(false)}>
+                  <FaPlay />
+                  Reproducir
+                </PrimaryButton>
+              </BackgroundPlayerButton>
+            )}
+            <Video
+              videoId={video?.id}
+              videoKey={video?.key}
+              isDecorative={isDecorative}
+              autoplay={true}
+            />
+          </>
+        )}
+      </BackgroundPlayer>
+      <DetailsContainer>
+        <DetailsMain>
+          <DetailsHeader>
+            <Title>{mediaDetails.title}</Title>
+            <Subtitle>{mediaDetails.tagline}</Subtitle>
+          </DetailsHeader>
+          <DetailsInfo>
+            <DetailsPoster>
+              <Poster
+                mediaItem={mediaDetails}
+                mediaType="movie"
+                isLink={false}
+                noWide={true}
+              />
+            </DetailsPoster>
+            <DetailsOverview>
+              <StatusTag>Status: {mediaDetails.status}</StatusTag>
+              <Subtitle>Descripción</Subtitle>
+              <Pharagraph>{mediaDetails.overview}</Pharagraph>
+              <Rating
+                value={mediaDetails.vote_average}
+                votes={mediaDetails.vote_count}
+              />
+              <DetailsOverviewGenres>
+                {mediaDetails.genres.map((genre, index) => (
+                  <Tag key={index}>{genre.name}</Tag>
+                ))}
+              </DetailsOverviewGenres>
+            </DetailsOverview>
+          </DetailsInfo>
+          <DetailsReviews>
+            <Subtitle>Comentarios</Subtitle>
+            {mediaReviews.map((review, index) => (
+              <Review key={index}>
+                <ReviewUser>
+                  <UserImage>
+                    <Image
+                      width="100%"
+                      height="100%"
+                      layout="responsive"
+                      objectFit="contain"
+                      quality={10}
+                      src={
+                        review.author_details.avatar_path
+                          ? review.author_details.avatar_path.includes(
+                              "secure.gravatar.com"
+                            )
+                            ? review.author_details.avatar_path.slice(1)
+                            : `https://image.tmdb.org/t/p/original${review.author_details.avatar_path}`
+                          : "https://localripplenet.com/images/default_user_img.jpg"
+                      }
+                      // placeholder="blur"
+                      // blurDataURL={`https://image.tmdb.org/t/p/original/${company.logo_path}`}
+                      alt={review.author_details.username}
+                    />
+                  </UserImage>
+                  <UserName>{review.author_details.username}</UserName>
+                </ReviewUser>
+                <Pharagraph>{review.content}</Pharagraph>
+              </Review>
+            ))}
+          </DetailsReviews>
+        </DetailsMain>
+        <DetailsAside>
+          <Companies>
+            {mediaDetails.production_companies.map((company, index) => (
+              <Company key={index} title={company.name}>
+                <CompanyImage>
+                  <Image
+                    width="200%"
+                    height="100%"
+                    layout="fixed"
+                    objectFit="contain"
+                    quality={10}
+                    src={`https://image.tmdb.org/t/p/original/${company.logo_path}`}
+                    // placeholder="blur"
+                    // blurDataURL={`https://image.tmdb.org/t/p/original/${company.logo_path}`}
+                    alt={company.name}
+                  />
+                </CompanyImage>
+                {/* <CompanyName>{company.name}</CompanyName> */}
+              </Company>
+            ))}
+          </Companies>
+          {/* <DetailsAsideVideos>
+              <Subtitle>Videos</Subtitle>
+              {mediaVideos.map((video, index) => (
+                <Video
+                  key={index}
+                  videoId={video?.id}
+                  videoKey={video?.key}
+                  isDecorative={false}
+                  autoplay={false}
+                />
+              ))}
+            </DetailsAsideVideos> */}
+        </DetailsAside>
+      </DetailsContainer>
+    </>
+  );
+};
+
+export default Index;
