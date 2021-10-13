ff087211f9a5b2cdc8b69ea1aaaecd73b13833c2
Puwiii <Sebastian.mathieur@gmail.com>
2021-10-13 19:24:23 -0300

    adding new styles to pharagraph component

--- components/atoms/Pharagraph.js
+++ components/atoms/Pharagraph.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 export const Pharagraph = styled.p`
-  font-size: 0.75rem;
-  font-weight: 300;
-  line-height: 1.6;
-  letter-spacing: 0.2px;
+  font-size: 0.87rem;
+  font-weight: 500;
+  line-height: 1.4;
+  /* letter-spacing: -0.2px; */
   color: ${({ theme }) => theme.color_text_200};
+  font-family: "Lato", sans-serif;
   display: -webkit-box;
   -webkit-line-clamp: ${({ crop }) => crop};
   -webkit-box-orient: vertical;
