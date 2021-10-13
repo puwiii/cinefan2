2d5a3b4f27505e863a81db20a8371b5eb9b0067d
Puwiii <Sebastian.mathieur@gmail.com>
2021-10-13 19:25:35 -0300

    adding new styles to Title component

--- components/atoms/Title.js
+++ components/atoms/Title.js
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 
 export const Title = styled.h2`
-  font-size: 2.13rem;
+  font-size: 1.8rem;
   letter-spacing: -0.5px;
+  font-weight: 600;
   /* font-family: "Lato", sans-serif; */
+
+  @media screen and (max-width: 768px) {
+    font-size: 1.47rem;
+  }
 `;
