8f02627d094ff9364e05ce3b11a2d06170ac9c36
Puwiii <Sebastian.mathieur@gmail.com>
2021-10-15 10:35:11 -0300

    clean theme file

--- components/themes.js
+++ components/themes.js
@@ -1,18 +1,33 @@
 export const darkTheme = {
-  color_blue_200: "#4a518d",
-  color_blue_300: "#343c7f",
-  color_blue_400: "#1d2671",
-  color_blue_500: "#171e5a",
-  color_blue_600: "#111744",
-  color_blue_700: "#0c0f2d",
+  color_primary_200: "#FFBB99",
+  color_primary_300: "#ff9966",
+  color_primary_400: "#FF8547",
+  color_primary_500: "#FF691F",
+  color_primary_600: "#F55200",
 
-  color_pink_200: "#d57393",
-  color_pink_300: "#cf5f83",
-  color_pink_400: "#c33764",
-  color_pink_500: "#9c2c50",
-  color_pink_600: "#75213c",
-  color_pink_700: "#4e1628",
+  color_secondary_200: "#FFADAF",
+  color_secondary_300: "#FF8587",
+  color_secondary_400: "#ff5e62",
+  color_secondary_500: "#FF474A",
+  color_secondary_600: "#E82E31",
 
+  // color_background_300: "#E4EBF1",
+  // color_background_400: "#eef2f6",
+
+  // color_text_400: "#2f3139",
+
+  color_text_200: "#cfcfcf",
+  color_text_400: "#fefefe",
+
+  color_background_300: "#212431",
+  color_background_400: "#181a23",
+
+  color_info: "#ffc107",
+  color_error: "#c43d3d",
+  color_success: "#21a047",
+};
+
+export const lightTheme = {
   color_primary_200: "#FFBB99",
   color_primary_300: "#ff9966",
   color_primary_400: "#FF8547",
@@ -32,13 +47,13 @@ export const darkTheme = {
 
   // color_text_400: "#2f3139",
 
-  color_text_200: "#cfcfcf",
-  color_text_400: "#fefefe",
+  color_background_300: "#efefef",
+  color_background_400: "#fefefe",
 
-  color_background_300: "#212431",
-  color_background_400: "#181a23",
+  color_text_200: "#212431",
+  color_text_400: "#181a23",
 
   color_info: "#ffc107",
   color_error: "#c43d3d",
-  color_success: "#21a047",
+  color_success: "#21a05f",
 };
